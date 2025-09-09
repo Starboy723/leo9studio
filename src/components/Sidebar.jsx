31c867255face61cbe7365644d64ffad39dce1d4
Gunda venkata sai <127464596+Starboy723@users.noreply.github.com>
2025-09-09 18:52:31 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar=()=>{
     return (
         <div className={`sidebar-container ${sideBar?"openSideBar":"closeSideBar"}`} id="sidebar-container">
         <section className="nav-section">
-        <svg className="close-btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 20L4 4m16 0L4 20"/></svg>
+        <svg className="close-btn" onClick={(e)=>setSideBar(false)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 20L4 4m16 0L4 20"/></svg>
         <h3 style={{color:"red"}}>Menu</h3>
         <ul>
             <li>Work</li>
