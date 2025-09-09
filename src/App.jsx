11f045df3303be4bb2815e5c2791c30c334efa5c
Gunda venkata sai <127464596+Starboy723@users.noreply.github.com>
2025-09-09 18:53:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes,Route } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar.jsx";
-import Services from "./components/services.jsx";
+import Services from "./components/Services.jsx";
 import services from "./utils/services.js";
 
 const HomePage=React.lazy(()=>import("./pages/HomePage.jsx"));
